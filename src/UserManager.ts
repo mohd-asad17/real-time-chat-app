26133def610176229c597fb5c614963cd0fbd366
mohd-asad17 <asadn01702@gmail.com>
2025-11-19 00:36:56 +0530

    message with zod validation

---
+++ src/UserManager.ts
@@ -0,0 +1,38 @@
+import { connection } from "websocket";
+
+interface User {
+  name: string;
+  id: string;
+  conn: connection;
+}
+interface Room {
+  users: User[];
+}
+export class UserManager {
+  private rooms: Map<String, Room>;
+  constructor() {
+    this.rooms = new Map<String, Room>();
+  }
+  addUser(name: string, userId: string, roomId: string, socket: connection) {
+    if (!this.rooms.get(roomId)) {
+      this.rooms.set(roomId, {
+        users: [],
+      });
+    }
+    this.rooms.get(roomId)?.users.push({
+      id: userId,
+      name,
+      conn: socket,
+    });
+  }
+  removeUser(roomId: string, userId: string) {
+    const users = this.rooms.get(roomId)?.users;
+    if (users) {
+      users.filter(({ id }) => id !== userId);
+    }
+  }
+  getUser(roomId: string, userId : string): User | null {
+    const user = this.rooms.get(roomId)?.users.find(({id}) => id === userId);
+    return user ?? null;
+  }
+}
